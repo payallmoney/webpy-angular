b71a16ab991a9338ec4c67c6dcf23e3638843989
payallmoney <payallmoney@163.com>
2013-04-28 19:54:51 +0800

    1,细小修改

---
+++ static/app/lib/angular-ui-grid/ng-grid-zh_cn.js
@@ -0,0 +1,14 @@
+window.ngGrid.i18n['en'] = {
+    ngAggregateLabel: '数据',
+    ngGroupPanelDescription: '将列头拖拽到此处进行分组.',
+    ngSearchPlaceHolder: '查询中...',
+    ngMenuText: '选择列:',
+    ngShowingItemsLabel: '显示数据:',
+    ngTotalItemsLabel: '总数:',
+    ngSelectedItemsLabel: '选中:',
+    ngPageSizeLabel: '页大小:',
+    ngPagerFirstTitle: '首页',
+    ngPagerNextTitle: '下一页',
+    ngPagerPrevTitle: '上一页',
+    ngPagerLastTitle: '尾页'
+};
